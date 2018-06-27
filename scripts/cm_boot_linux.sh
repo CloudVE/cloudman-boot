c0ae7cb8eceb69bb145a07b7b3993f972ebb2df4
Nuwan Goonasekera <nuwan.ag@gmail.com>
2018-06-26 19:13:41 -0700

    Accept host name as a parameter

--- scripts/cm_boot_linux.sh
+++ scripts/cm_boot_linux.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker run -v /var/run/docker.sock:/var/run/docker.sock -e RANCHER_SERVER=127.0.0.1 --net=host galaxy/cloudman-boot
+HOST_NAME=${1:-127.0.0.1}
+docker run -v /var/run/docker.sock:/var/run/docker.sock -e "RANCHER_SERVER=$HOST_NAME" --net=host galaxy/cloudman-boot
