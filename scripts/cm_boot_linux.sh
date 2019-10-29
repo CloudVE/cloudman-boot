5087eff42739c4c084a2512a9a68ace484a01d9a
Ubuntu <ubuntu@am-cloudman-boot.jetstreamlocal>
2019-10-29 19:28:59 +0000

    Pin rancher, change name in script

--- scripts/cm_boot_linux.sh
+++ scripts/cm_boot_linux.sh
@@ -4,4 +4,4 @@ if [ $HOST_NAME = '--use-public-ip' ]
 then
    HOST_NAME=$(curl http://whatismyip.akamai.com)
 fi
-docker run -v /var/run/docker.sock:/var/run/docker.sock -e "RANCHER_SERVER=$HOST_NAME" --net=host galaxy/cloudman-boot
+docker run -v /var/run/docker.sock:/var/run/docker.sock -e "RANCHER_SERVER=$HOST_NAME" --net=host almahmoud/cloudman-boot:test
