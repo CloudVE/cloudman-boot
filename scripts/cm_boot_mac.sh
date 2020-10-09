a2f7c2b64f4de893eaf70e0c7ee3e9d575354a27
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2020-10-09 20:13:15 +0530

    Rename rancher_server to cluster_hostname and rancher_pwd to cluster_password

--- scripts/cm_boot_mac.sh
+++ scripts/cm_boot_mac.sh
@@ -4,4 +4,4 @@ docker-machine create --driver virtualbox --virtualbox-memory $VM_MEMORY cloudma
 eval $(docker-machine env cloudman)
 DOCKER_VM_IP=$(echo $DOCKER_HOST| awk -F/  '{print $3}' | cut -d: -f1)
 echo $DOCKER_VM_IP
-docker run -v /var/run/docker.sock:/var/run/docker.sock -e RANCHER_SERVER=$DOCKER_VM_IP galaxy/cloudman-boot
+docker run -v /var/run/docker.sock:/var/run/docker.sock -e CLUSTER_HOSTNAME=$DOCKER_VM_IP galaxy/cloudman-boot
