c05b29fd405be9a6a3a3557e085114be6d1af7d6
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2020-12-04 21:55:45 +0530

    Set instance tag directly through boto IAM

--- roles/rke/files/set_aws_instance_tag.py
+++ roles/rke/files/set_aws_instance_tag.py
@@ -1,52 +1,33 @@
 #!/usr/bin/python3
 import argparse
-import base64
-import os
-import yaml
+import sys
 
-from cloudbridge.factory import CloudProviderFactory, ProviderList
+import boto3
 
-
-def get_bootstrap_data():
-    bootstrap_data = os.environ.get('CM_INITIAL_CLUSTER_DATA')
-    # Pad data: https://gist.github.com/perrygeo/ee7c65bb1541ff6ac770
-    data = base64.b64decode(bootstrap_data + "===").decode('utf-8')
-    return yaml.safe_load(data)
+import requests
 
 
-def get_provider_config(json_data):
-    config = json_data.get('cloud_config')
-    target = config.get('target')
-    zone = target.get('target_zone')
-    cloud = zone.get('cloud')
-    region = zone.get('region')
-    credentials = config.get('credentials', {})
-
-    provider_config = {}
-    provider_config.update(zone)
-    provider_config.update(region)
-    provider_config.update(cloud)
-    provider_config.update(credentials)
-
-    return provider_config
+def set_instance_tags(region_name, instance_id, tags):
+    ec2 = boto3.resource('ec2', region_name=region_name)
+    ec2_instance = ec2.Instance(instance_id)
+    ec2_instance.create_tags(
+        Tags=tags)
 
 
 def main(cluster_id):
-    cloudlaunch_data = get_bootstrap_data()
-    provider_config = get_provider_config(cloudlaunch_data)
+    response = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
+    instance_id = response.text
+    response = requests.get('http://169.254.169.254/latest/meta-data/placement/region')
+    region_name = response.text
+    set_instance_tags(region_name, instance_id,
+                      [{'Key': f'kubernetes.io/cluster/{cluster_id}',
+                        'Value': "owned"}])
+    return 0
 
-    provider = CloudProviderFactory().create_provider(ProviderList.AWS, provider_config)
-    inst_id = cloudlaunch_data.get('host_config', {}).get('instance_id')
-    this_inst = provider.compute.instances.get(inst_id)
-    if this_inst:
-        # pylint:disable=protected-access
-        this_inst._ec2_instance.create_tags(
-            Tags=[{'Key': f'kubernetes.io/cluster/{cluster_id}',
-                   'Value': "owned"}])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Set AWS instance tag')
     parser.add_argument('cluster_id', help='cluster id of the kubernetes/rancher cluster')
     args = parser.parse_args()
 
-    main(args.cluster_id)
+    sys.exit(main(args.cluster_id))
